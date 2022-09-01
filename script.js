7109c5c8fe6e1e837790cddffdae6c0876dcdb66
parmodpk23 <101690777+parmodpk23@users.noreply.github.com>
2022-08-31 22:39:13 -0400

    Aug/31/2022

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let deleteid = "deleteid";
+let deleteid = "did";
 function addid() {
     deleteid = deleteid + 1;
     return deleteid;
@@ -11,8 +11,8 @@ function addingItem() {
     else{    
     document.getElementById("banner").style="display:none";
     document.getElementById("todo").value="";
-    document.getElementById("autolist").innerHTML += `<li class="clearlist" id=${deleteid}><p>${itemName}</p> <button id=${deleteid} onclick="deletelist()">delete</button></li>`;
-    deleteid = deleteid + 1;
+    document.getElementById("autolist").innerHTML += `<li class="clearlist" id=${deleteid}><p>${itemName}</p> <button id=${deleteid} onclick="deletelist(this.id)">delete</button></li>`;
+    console.log(deleteid);
     return deleteid;
     }
 }
@@ -24,10 +24,12 @@ function clearItem(){
 document.getElementById("banner").style="display:block";
 }
 
-function deletelist() {
-    let clr = document.getElementById("autolist");
-
-    let abc=document.getElementsByTagName('li')[0].id;
-    document.getElementById(abc).remove();
-    document.getElementById("banner").style="display:block";
+function deletelist(dl) {
+   document.getElementById(dl).remove();
+       //  document.getElementById("banner").style="display:block";
+    //let clr = document.getElementById("clearlist");
+    //let abc=document.getElementsByTagName('button')[0].id;
+    //console.log(dl);
+    //console.log(abc);
+   // document.getElementById("banner").style="display:block";
 }
